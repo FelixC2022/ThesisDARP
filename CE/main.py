2c3a3467942dc2afc5034bc789b9e2c96a9e405d
FelixC <cornelis.felix@gmail.com>
2022-03-28 11:29:12 +0200

    make branch named felix the new master

--- CE/main.py
+++ CE/main.py
@@ -3,3 +3,8 @@ from load_instnace import load_instance
 path = 'data/a2-16.txt'
 K, n, T, Q, L, x_co, y_co, s, cap, e, l  = load_instance(path)
 
+
+#In total there are 16 users which need to be served 
+route = [0, 1, 2, 1+n, 2+n, 2*n+1]
+
+print(route,n)
